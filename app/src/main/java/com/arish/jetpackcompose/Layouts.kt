915ac31e8b62822c0aed5a3bf4287fbb96109124
haris afaq <haris.afaq@inara.pk>
2025-11-14 12:24:50 +0500

    "Column Compose (Widget) in jetpack Compose"

--- app/src/main/java/com/arish/jetpackcompose/Layouts.kt
+++ app/src/main/java/com/arish/jetpackcompose/Layouts.kt
@@ -1,2 +1,35 @@
 package com.arish.jetpackcompose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ColumnWidget(modifier: Modifier = Modifier) {
+Column (modifier = Modifier.fillMaxSize(),
+    horizontalAlignment =  Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+    ){
+    Text("Column Widget")
+    Spacer(modifier = Modifier.height(15.dp))
+    Button(onClick = {}) {
+        Text("Button")
+    }
+}
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun ColumnLayoutPreview(modifier: Modifier = Modifier) {
+    ColumnWidget()
+}
